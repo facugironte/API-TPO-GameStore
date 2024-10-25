16fa6d4f96392ff4fc02966efd1f9d6cd43a186f
Puleio <spuleio@uade.edu.ar>
2024-10-25 15:43:34 -0300

    front-empresa

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -9,9 +9,14 @@ import Library from "./pages/Library/Library";
 import Cart from "./pages/Cart/Cart";
 import Purchase from "./pages/Purchase/Purchase";
 import NewPaymentMethod from "./pages/NewPaymentMethod/NewPaymentMethod";
-import Forum from "./pages/Forum/Forum";
+import Forum from "./pages/user/Forum/Forum";
 import Profile from "./pages/Profile/Profile";
 import Wishlist from "./pages/Wishlist/Wishlist";
+import PerfilEmpresa from "./pages/company/company-profile/PerfilEmpresa";
+import NuevoJuego from "./pages/company/new-game/NuevoJuego";
+import JuegosEmpresa from "./pages/company/your-games/JuegosEmpresa";
+import EstadisticasEmpresa from "./pages/company/stats/EstadisticasEmpresa";
+import Modificadores from "./pages/company/modify/Modificadores";
 
 import "./styles.css";
 
@@ -63,6 +68,26 @@ const App = () => {
     {
       path: "/shop/payment-method",
       element: <NewPaymentMethod />
+    },
+    {
+      path: "/company-profile",
+      element: <PerfilEmpresa />
+    },
+    {
+      path: "/new-game",
+      element: <NuevoJuego />
+    },
+    {
+      path: "/company-your-games",
+      element: <JuegosEmpresa/>
+    },
+    {
+      path: "/stats",
+      element: <EstadisticasEmpresa/>
+    },
+    {
+      path: "/company-modify-game",
+      element: <Modificadores/>
     }
   ])
 
